df605ac0bd9893eb46093ccfd7ab5eccc3c771a0
hectordguini-hub <hectordguini@gmail.com>
2026-07-30 08:21:08 -0300

    Change SUPABASE_ANON_KEY in config.js
    Updated SUPABASE_ANON_KEY to a new value.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SUPABASE_URL: "https://ebzqgkdttopcwtacuhlx.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_...", // el valor completo que copiaste
+  SUPABASE_ANON_KEY: "sb_publishable_DPZOvrOfxGMZF83t8edZng_5UeUXgOt",
   BACKEND_URL: "https://matriz-recupero-backend.onrender.com",
 };
